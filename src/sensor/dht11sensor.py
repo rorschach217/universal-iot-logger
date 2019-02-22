3995749699892aa8343a5b5ccf74f54943df15ed
Gyan Panchal <kg21p1@gmail.com>
2019-02-22 10:37:10 +0530

    dht sensor file updated.

--- src/sensor/dht11sensor.py
+++ src/sensor/dht11sensor.py
@@ -1,17 +1,22 @@
 import RPi.GPIO as GPIO
-import dht11
+# import dht11
 import sys, os
 
 sys.path.append(os.path.dirname(os.path.realpath('src/api/api.py')))
 
 import api as api
 
-instance = dht11.DHT11(pin=4)
+# instance = dht11.DHT11(pin=4)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+result=GPIO.input(4)
 
 def readSensorData(parameter):
     log_temp=dict()
     log_humi=dict()
-    result = instance.read()
+    # result = instance.read()
+    print result
     if result.is_valid():
         log_temp["deviceId"]= "TEMPSENSE"
         log_temp["logValue"]= result.temperature
