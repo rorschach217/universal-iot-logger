82918748ad426039e2a50290416e480ff9d09e9f
Gyan Panchal <kg21p1@gmail.com>
2019-04-05 19:58:16 +0530

    updated

--- src/sensor/flamesensor.py
+++ src/sensor/flamesensor.py
@@ -13,18 +13,19 @@ import api as api
 
 def readSensorData(GPIO):
     GPIO.setup(22, GPIO.IN)
+    result=GPIO.input(22)
     log_flame=dict()
-    if GPIO.input(22).is_valid():
-    	if GPIO.input(22)==False: #When output from motion sensor is LOW
+    if result.is_valid():
+    	if result==False: #When output from motion sensor is LOW
 	        log_flame["deviceId"]= "FLAME"
-        	log_flame["logValue"]= GPIO.input(22)
+        	log_flame["logValue"]= result
         	log_flame["deviceType"]= "flame"
         	log_flame["valuePrefix"]= "FLAME NOT DETECTED"
         	log_flame["departmentId"]= "PLANT"
         	log_flame["createdBy"]= "b8:27:eb:4a:65:3c"
         else: #When output from motion sensor is HIGH
         	log_flame["deviceId"]= "FLAME"
-        	log_flame["logValue"]= GPIO.input(22)
+        	log_flame["logValue"]= result
         	log_flame["deviceType"]= "flame"
         	log_flame["valuePrefix"]= "FLAME DETECTED"
         	log_flame["departmentId"]= "PLANT"
