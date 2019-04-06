f36d495d59a5ca6d55ec2a50d77caed46073aa9c
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 11:42:14 +0530

    updated

--- src/sensor/flamesensor.py
+++ src/sensor/flamesensor.py
@@ -6,20 +6,20 @@ import api as api
 
 def readSensorData(result):
     log_flame=dict()
-	if result==False:
+    if result==False:
         log_flame["deviceId"]= "FLAME"
-    	log_flame["logValue"]= 1
-    	log_flame["deviceType"]= "flame"
-    	log_flame["valuePrefix"]= "FLAME DETECTED"
-    	log_flame["departmentId"]= "PLANT"
-    	log_flame["createdBy"]= "b8:27:eb:4a:65:3c"
+        log_flame["logValue"]= 1
+        log_flame["deviceType"]= "flame"
+        log_flame["valuePrefix"]= "FLAME DETECTED"
+        log_flame["departmentId"]= "PLANT"
+        log_flame["createdBy"]= "b8:27:eb:4a:65:3c"
     else:
-    	log_flame["deviceId"]= "FLAME"
-    	log_flame["logValue"]= 0
-    	log_flame["deviceType"]= "flame"
-    	log_flame["valuePrefix"]= "FLAME NOT DETECTED"
-    	log_flame["departmentId"]= "PLANT"
-    	log_flame["createdBy"]= "b8:27:eb:4a:65:3c"
+        log_flame["deviceId"]= "FLAME"
+        log_flame["logValue"]= 0
+        log_flame["deviceType"]= "flame"
+        log_flame["valuePrefix"]= "FLAME NOT DETECTED"
+        log_flame["departmentId"]= "PLANT"
+        log_flame["createdBy"]= "b8:27:eb:4a:65:3c"
 
     api.postDataToServer(log_flame)
 
