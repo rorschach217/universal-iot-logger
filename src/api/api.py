937625d36007082f8bee6a94aaa0e49a7e95de79
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 18:42:45 +0530

    updated

--- src/api/api.py
+++ src/api/api.py
@@ -2,8 +2,10 @@ import requests
 import sys, os
 
 sys.path.append(os.path.dirname(os.path.realpath('src/constants.py')))
+sys.path.append(os.path.dirname(os.path.realpath('src/db/database.py')))
 
 import constants as constant
+import database as database
 
 logURL = constant.baseurl + "/logs"
 errorLogURL = constant.baseurl + "/error/logs"
@@ -13,6 +15,9 @@ def postDataToServer(data):
     request = requests.post(logURL, data=data)
     if request.status_code != 200:
         print "Error:", request.status_code
+        database.insertDataIntoDatabase(data)
+    else:
+        database.checkDataExistInDatabase()
     return
 
 def readDataFromServer():
--- src/db/database.py
+++ src/db/database.py
@@ -1,1 +1,25 @@
 import sqlite3
+import time;
+connection = sqlite3.connect("locallogs")
+cursor = connection.cursor()
+
+def createTable():
+    cursor.execute("CREATE TABLE logs (timestamp TEXT, deviceId TEXT, logValue TEXT, deviceType TEXT, valuePrefix TEXT, departmentId TEXT, createdBy TEXT)")
+    connection.commit()
+    return True
+
+def checkTableExist():
+    cursor.execute("SELECT COUNT(*) FROM sqlite_master WHERE type = ? AND name = ?", new String[] {"table", "logs"})
+    if (!cursor.moveToFirst()):
+        return False
+    return True
+
+def insertDataIntoDatabase(data):
+    cursor.execute("INSERT INTO logs (timestamp, deviceId, logValue, deviceType, valuePrefix, departmentId, createdBy) VALUES (?, ?, ?, ?, ?, ?, ?)", (time.time() ,data["deviceId"], data["logValue"], data["deviceType"], data["valuePrefix"], data["departmentId"], data["createdBy"]))
+    connection.commit()
+    return True
+
+def checkDataExistInDatabase(data):
+    cursor.execute("SELECT * FROM logs")
+    for row in cursor:
+        print row
