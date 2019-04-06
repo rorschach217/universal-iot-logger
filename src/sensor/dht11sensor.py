e4641ee483a5328c8519943b1c41b1e9956d23c1
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 12:04:24 +0530

    updated

--- src/sensor/dht11sensor.py
+++ src/sensor/dht11sensor.py
@@ -4,9 +4,8 @@ sys.path.append(os.path.dirname(os.path.realpath('src/api/api.py')))
 
 import api as api
 
-def readSensorData(result):
+def temperatureData(result):
     log_temp=dict()
-    log_humi=dict()
     if result.is_valid():
         log_temp["deviceId"]= "TEMPSENSE"
         log_temp["logValue"]= result.temperature
@@ -14,13 +13,22 @@ def readSensorData(result):
         log_temp["valuePrefix"]= "degree"
         log_temp["departmentId"]= "PLANT"
         log_temp["createdBy"]= "b8:27:eb:4a:65:3c"
+    if log_temp:
+        api.postDataToServer(log_temp)
 
+def humidityData(result):
+    log_humi=dict()
+    if result.is_valid():
         log_humi["deviceId"]= "HUMISENSE"
         log_humi["logValue"]= result.humidity
         log_humi["deviceType"]= "humidity"
         log_humi["valuePrefix"]= "%"
         log_humi["departmentId"]= "PLANT"
         log_humi["createdBy"]= "b8:27:eb:4a:65:3c"
-    api.postDataToServer(log_temp)
-    api.postDataToServer(log_humi)
+    if log_temp:
+        api.postDataToServer(log_humi)
+
+def readSensorData(result):
+    temperatureData(result)
+    humidityData(result)
     return
