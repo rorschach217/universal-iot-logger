1ec55eee9d75ff2683791d4d3fe31a3f1b147938
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 17:57:58 +0530

    updated

--- src/sensor/dht11sensor.py
+++ src/sensor/dht11sensor.py
@@ -6,13 +6,13 @@ import api as api
 
 def readTemperatureSensorData(result):
     log_temp=dict()
-    # if result.is_valid():
-    #     log_temp["deviceId"]= "TEMPSENSE"
-    #     log_temp["logValue"]= result.temperature
-    #     log_temp["deviceType"]= "temperature"
-    #     log_temp["valuePrefix"]= "degree"
-    #     log_temp["departmentId"]= "PLANT"
-    #     log_temp["createdBy"]= "b8:27:eb:4a:65:3c"
+    if result.is_valid():
+        log_temp["deviceId"]= "TEMPSENSE"
+        log_temp["logValue"]= result.temperature
+        log_temp["deviceType"]= "temperature"
+        log_temp["valuePrefix"]= "degree"
+        log_temp["departmentId"]= "PLANT"
+        log_temp["createdBy"]= "b8:27:eb:4a:65:3c"
     if log_temp:
         api.postDataToServer(log_temp)
     else:
@@ -20,13 +20,13 @@ def readTemperatureSensorData(result):
 
 def readhumiditySensorData(result):
     log_humi=dict()
-    # if result.is_valid():
-    #     log_humi["deviceId"]= "HUMISENSE"
-    #     log_humi["logValue"]= result.humidity
-    #     log_humi["deviceType"]= "humidity"
-    #     log_humi["valuePrefix"]= "%"
-    #     log_humi["departmentId"]= "PLANT"
-    #     log_humi["createdBy"]= "b8:27:eb:4a:65:3c"
+    if result.is_valid():
+        log_humi["deviceId"]= "HUMISENSE"
+        log_humi["logValue"]= result.humidity
+        log_humi["deviceType"]= "humidity"
+        log_humi["valuePrefix"]= "%"
+        log_humi["departmentId"]= "PLANT"
+        log_humi["createdBy"]= "b8:27:eb:4a:65:3c"
     if log_humi:
         api.postDataToServer(log_humi)
     else:
