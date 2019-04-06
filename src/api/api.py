6927ed2efb4bda2b7a1bdd37c19ff99855dd9003
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 17:54:53 +0530

    updated

--- src/api/api.py
+++ src/api/api.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.realpath('src/constants.py')))
 import constants as constant
 
 logURL = constant.baseurl + "/logs"
+errorLogURL = constant.baseurl + "/error/logs"
 sensorURL = constant.baseurl + "/sensors"
 
 def postDataToServer(data):
@@ -21,3 +22,14 @@ def readDataFromServer():
         return request
     else:
         return request.json()
+
+def postErrorToServer(message, sensor, deviceId, mac):
+    error=dict()
+    error["deviceId"]= deviceId
+    error["sensor"]= sensor
+    error["message"]= message
+    error["createdBy"]= mac
+    request = requests.post(errorLogURL, data=error)
+    if request.status_code != 200:
+        print "Error:", request.status_code
+    return
--- src/sensor/dht11sensor.py
+++ src/sensor/dht11sensor.py
@@ -6,24 +6,28 @@ import api as api
 
 def readTemperatureSensorData(result):
     log_temp=dict()
-    if result.is_valid():
-        log_temp["deviceId"]= "TEMPSENSE"
-        log_temp["logValue"]= result.temperature
-        log_temp["deviceType"]= "temperature"
-        log_temp["valuePrefix"]= "degree"
-        log_temp["departmentId"]= "PLANT"
-        log_temp["createdBy"]= "b8:27:eb:4a:65:3c"
+    # if result.is_valid():
+    #     log_temp["deviceId"]= "TEMPSENSE"
+    #     log_temp["logValue"]= result.temperature
+    #     log_temp["deviceType"]= "temperature"
+    #     log_temp["valuePrefix"]= "degree"
+    #     log_temp["departmentId"]= "PLANT"
+    #     log_temp["createdBy"]= "b8:27:eb:4a:65:3c"
     if log_temp:
         api.postDataToServer(log_temp)
+    else:
+        api.postErrorToServer("TEMPSENSE", "temperature", "Sensor not responding", "b8:27:eb:4a:65:3c")
 
 def readhumiditySensorData(result):
     log_humi=dict()
-    if result.is_valid():
-        log_humi["deviceId"]= "HUMISENSE"
-        log_humi["logValue"]= result.humidity
-        log_humi["deviceType"]= "humidity"
-        log_humi["valuePrefix"]= "%"
-        log_humi["departmentId"]= "PLANT"
-        log_humi["createdBy"]= "b8:27:eb:4a:65:3c"
+    # if result.is_valid():
+    #     log_humi["deviceId"]= "HUMISENSE"
+    #     log_humi["logValue"]= result.humidity
+    #     log_humi["deviceType"]= "humidity"
+    #     log_humi["valuePrefix"]= "%"
+    #     log_humi["departmentId"]= "PLANT"
+    #     log_humi["createdBy"]= "b8:27:eb:4a:65:3c"
     if log_humi:
         api.postDataToServer(log_humi)
+    else:
+        api.postErrorToServer("HUMISENSE", "humidity", "Sensor not responding", "b8:27:eb:4a:65:3c")
