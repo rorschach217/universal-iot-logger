3f885b1504dd246ec842a91037608d602a939e4f
Gyan Panchal <kg21p1@gmail.com>
2019-04-06 12:59:00 +0530

    updated

--- default.py
+++ default.py
@@ -20,25 +20,23 @@ GPIO.cleanup()
 while True:
     sensors = api.readDataFromServer()
     for sensor in sensors:
-        print type(sensor)
-        print sensor
         print sensor["pin"]
-        print type(sensor.pin)
+        print type(sensor["pin"])
         if sensor.sensor == "temperature" and sensor.isEnabled == True:
-            dht11 = dht11.DHT11(pin=sensor.pin)
+            dht11 = dht11.DHT11(pin=sensor["pin"])
             dht11sensor.readTemperatureSensorData(dht11.read())
         if sensor.sensor == "humidity" and sensor.isEnabled == True:
-            dht11 = dht11.DHT11(pin=sensor.pin)
+            dht11 = dht11.DHT11(pin=sensor["pin"])
             dht11sensor.readhumiditySensorData(dht11.read())
         if sensor.sensor == "infrared" and sensor.isEnabled == True:
-            GPIO.setup(sensor.pin, GPIO.IN)
-            irsensor.readSensorData(GPIO.input(sensor.pin))
+            GPIO.setup(sensor["pin"], GPIO.IN)
+            irsensor.readSensorData(GPIO.input(sensor["pin"]))
         if sensor.sensor == "gas" and sensor.isEnabled == True:
-            GPIO.setup(sensor.pin, GPIO.IN)
-            gassensor.readSensorData(GPIO.input(sensor.pin))
+            GPIO.setup(sensor["pin"], GPIO.IN)
+            gassensor.readSensorData(GPIO.input(sensor["pin"]))
         if sensor.sensor == "Flame" and sensor.isEnabled == True:
-            GPIO.setup(sensor.pin, GPIO.IN)
-            flamesensor.readSensorData(GPIO.input(sensor.pin))
+            GPIO.setup(sensor["pin"], GPIO.IN)
+            flamesensor.readSensorData(GPIO.input(sensor["pin"]))
     # flamesensor.readSensorData(GPIO.input(22))
     # gassensor.readSensorData(GPIO.input(27))
     # irsensor.readSensorData(GPIO.input(17))
